7d83e975f4d052d4051250da0c341f8ee2193e2b
Raymond <dlwhdgus482@gmail.com>
2019-07-20 17:00:07 +0900

    Counter, Player, Header 컴포넌트를 모듈로 분리

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./components/Header";
+import {Player} from "./components/Player";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    players: [
+      {name: 'LDK', score: 30, id: 1},
+      {name: 'HONG', score: 40, id: 2},
+      {name: 'KIM', score: 50, id: 3},
+      {name: 'PARK', score: 60, id: 4},
+    ]
+  }
+
+  render() {
+    return (
+        <div className="scoreboard">
+          <Header title="My Scoreboard" totalPlayers={11} />
+
+          {
+            this.state.players.map((player) =>
+                <Player name={player.name} score={player.score}
+                        id={player.id} key={player.id}
+                        removePlayer={this.handleRemovePlayer}/>)
+          }
+        </div>
+    )
+  }
+
+  handleRemovePlayer = (id) => {
+    console.log(id);
+    // 자식을 삭제하는 로직
+    this.setState(prevState => ({
+          players: prevState.players.filter(player => player.id !== id)
+        })
+    )
+  }
 }
 
 export default App;

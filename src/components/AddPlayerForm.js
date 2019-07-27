080bffe9676b4a63f13c868a723e1ff2916a721f
Raymond <dlwhdgus482@gmail.com>
2019-07-27 10:23:49 +0900

    AddPlayerForm - player 추가 로직 구현(자식이 부모와 통신)

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -9,11 +9,17 @@ export class AddPlayerForm extends React.Component {
         this.setState({value: e.target.value})
     }
 
+    handleSubmit = (e) => {
+        // 화면을 리프레쉬하는 기본 이벤트 막기
+        e.preventDefault();
+        this.props.addPlayer(this.state.value);
+    }
+
     render() {
         return (
             <form action="" className="form">
                 <input type="text" className="input" placeholder="Enter a player's name" value={this.state.value} onChange={this.handleValueChange}/>
-                <input type="submit" className="input" value="Add Player"/>
+                <input type="submit" className="input" value="Add Player" onClick={this.handleSubmit}/>
             </form>
         );
     }
